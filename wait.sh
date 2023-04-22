313c9d60ceffd4abd374963dc76519eb460f28e2
David Chung <dchung@dchung.com>
2023-04-21 22:41:07 -0500

    updated wait.sh

--- wait.sh
+++ wait.sh
@@ -16,7 +16,7 @@ echo ""
 #echo "Choose Astra DB or Cassandra on the left panel to start."
 
 echo -n 'Starting a Cassandra cluster ...'; 
-timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
+timeout 60 bash -c 'until /workspace/ds201-lab06/apache-cassandra-4.1.0/bin/cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
 sleep 2
 echo ' DONE!'
 echo "Cassandra successfully started."
